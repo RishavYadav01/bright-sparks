3c69e29bd10dc487f4bdcaf7ca2be63f2e2ca748
RishavYadav01 <rishavyadav87421@gmail.com>
2026-06-20 18:51:37 +0530

    Fix frontend deployment issue

--- bright-sparks-frontend/src/services/api.js
+++ bright-sparks-frontend/src/services/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-export default axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+const API = axios.create({
+    baseURL: "https://bright-sparks-production.up.railway.app/api"
 });
+
+export default API;
